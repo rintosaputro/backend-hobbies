6bca6b2bbbc5db9ed84ac8223b3cc67da8389ac5
rintosaputro <saputrorinto@gmail.com>
2022-06-15 11:33:27 +0700

    feat: init

---
+++ src/app.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const cors = require('cors')
+const morgan = require('morgan')
+
+const {PORT} = process.env
+
+const app = express()
+
+app.use(express.urlencoded({extended: true}))
+app.use(cors())
+app.use(morgan('dev'))
+app.use('/', require('./routes'))
+
+app.listen(PORT, () => console.log(`App listen on port ${PORT}`))
