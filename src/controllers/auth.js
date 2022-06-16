c90da141e7eebb5d544aa00773ababb0de9864b0
rintosaputro <saputrorinto@gmail.com>
2022-06-16 22:25:09 +0700

    feat: add login

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -2,6 +2,9 @@ const Users = require('../models/users')
 const {response} = require('../helpers/responseHandler')
 const {passwordValidator} = require('../helpers/validator')
 const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
+
+const {APP_SECRET} = process.env
 
 exports.register = async (req, res) => {
   try {
@@ -20,6 +23,47 @@ exports.register = async (req, res) => {
     })
 
     return response(res, 'Successfully created user')
+  } catch (err) {
+    if (err.errors) {
+      let message = ''
+      err.errors.map(error => {
+        message = error.message
+      })
+
+      return response(res, message, null, null, 400)
+    } else {
+      return response(res, 'Unexpeted Error', null, null, 500)
+    }
+  }
+}
+
+exports.login = async (req, res) => {
+  try {
+    const {email, password} = req.body
+
+    if (!email && !password) {
+      return response(res, 'Email and password can not be empty')
+    }
+
+    const user = await Users.findOne({
+      where: {email}
+    })
+
+    if (!user) {
+      return response(res, 'User with this email is not found', null, null, 400)
+    }
+
+    const checkPassword = await bcrypt.compare(password, user.password)
+
+    if (!checkPassword) {
+      return response(res, 'Invalid password', null, null, 400)
+    }
+
+    const {id} = user
+    const token = jwt.sign({id}, APP_SECRET)
+    const results = {token}
+
+    return response(res, 'Successfully logged in', results)
   } catch (err) {
     if (err.errors) {
       let message = ''
